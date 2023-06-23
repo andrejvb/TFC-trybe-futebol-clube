ecc08ea6c6af01f135e53d9e34862513ff6823d9
andrejvb <andre.jvb@gmail.com>
2023-06-23 18:24:42 -0300

    second flow tests

---
+++ app/backend/src/tests/mock/users.mock.ts
@@ -0,0 +1,8 @@
+const validLogin = {
+    email: 'admin@admin.com',
+    password: 'secret_admin'
+};
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY4NzU1MTYwNCwiZXhwIjoxNjg4MTU2NDA0fQ.xPTch7UzGylobiyAZLwEZkVJiEc1WO7G7OsjFQy6Ybc'
+  
+export default { validLogin, token }
---
+++ app/backend/src/tests/users.test.ts
@@ -0,0 +1,48 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { App } from '../app';
+import SequelizeTeam from '../database/models/SequelizeTeam';
+import teamsMock from './mock/teams.mock';
+import usersMock from './mock/users.mock';
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
+
+let token;
+describe('Sucess Case', () => {    
+    
+    beforeEach(sinon.restore);
+    
+  it('login whith sucess', async () => {
+    // Arrange
+    const validUser = usersMock.validLogin
+
+    // Act
+    const { status, body } = await chai.request(app).post('/login').send(validUser);
+    // Assert
+    expect(status).to.be.equal(200);
+    expect(body).to.have.own.property('token')
+
+  });
+
+  it('Validation user whith sucess', async () => {
+    // Arrange
+
+    // Act
+    const { status, body } = await chai.
+    request(app)
+    .get('/login/role')
+    .set('Authorization', usersMock.token)
+        
+    // Assert
+    expect(status).to.be.equal(200);
+    expect(body).to.have.own.property('role')
+    expect(body).to.have.deep.property('role', 'admin');
+  });
+});
